0b0b81a79ef0704e7590f6f85511a5d1571bf026
oresendis <resendis@ccg.unam.mx>
2021-07-24 19:17:13 -0500

    Add files via upload

---
+++ Machine_Learning/XGBoost_Model/CompleteModel_confusionMatrix.py
@@ -0,0 +1,48 @@
+
+# Load validation data. Check that you are in a folder with load_libraries
+from load_libraries import *
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+def myfun(self=None):
+    return model_bytearray
+
+pickle_in = open("/media/usb/osbaldo/COVID-19/singlecell/paper_corrected/Model/model.pickle","rb")
+model = pickle.load(pickle_in);
+
+#Load the files test
+pickle_in = open("/media/usb/osbaldo/COVID-19/singlecell/paper_corrected/Splited_data/X_test.pickle","rb")
+X_test = pickle.load(pickle_in);
+
+pickle_in = open("/media/usb/osbaldo/COVID-19/singlecell/paper_corrected/Splited_data/y_test.pickle","rb")
+y_test = pickle.load(pickle_in);
+
+
+pickle_in = open("/media/usb/osbaldo/COVID-19/singlecell/paper_corrected/validationData/dat_val/X_val.pickle","rb")
+X_val = pickle.load(pickle_in);
+
+pickle_in = open("/media/usb/osbaldo/COVID-19/singlecell/paper_corrected/validationData/dat_val/y_val.pickle","rb")
+y_val = pickle.load(pickle_in);
+
+
+# Write in D format
+D_test = xgb.DMatrix(X_test, label=y_test);
+D_val = xgb.DMatrix(X_val, label=y_val);
+
+
+# Performance of the model evaluating the test dataset.
+pred_test= model.predict(D_test)
+pred_val= model.predict(D_val)
+
+print(classification_report(y_test, pred_test))
+cm_test = confusion_matrix(y_test, pred_test)
+cm_val = confusion_matrix(y_val, pred_val)
+
+## To generate ONLY the confusion matrix with labels
+cmap = plt.get_cmap('Blues')
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm_test,display_labels=['Moderate','Severe']).plot(cmap = cmap,colorbar=False)
+plt.savefig('confusematrix_test.png',dpi=800)
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm_val,display_labels=['Moderate','Severe']).plot(cmap = cmap,colorbar=False)
+plt.savefig('confusematrix_val.png',dpi=800)
+
